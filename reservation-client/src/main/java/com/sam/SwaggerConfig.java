0202d5ed49f1406a6186c1d64614e3ba72f6e1f2
sandeep <sandeepgoud0612@gmail.com>
2018-03-05 22:04:21 -0500

    Swagger Updates

--- reservation-client/src/main/java/com/sam/SwaggerConfig.java
+++ reservation-client/src/main/java/com/sam/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package com.sam;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -15,6 +19,16 @@ public class SwaggerConfig {
 	public Docket configure() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any()).build();
+	}
+	
+	private ApiInfo apiInfo() {
+	     return new ApiInfo(
+	       "My REST API", 
+	       "Some custom description of API.", 
+	       "API TOS", 
+	       "Terms of service", 
+	       new Contact("John Doe", "www.example.com", "myeaddress@company.com"), 
+	       "License of API", "API license URL", Collections.emptyList());
 	}
 
 }
